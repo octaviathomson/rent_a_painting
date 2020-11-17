5561b690581636c1ab24fd7a889d645e1f90bcf8
albane tonnellier <albanetonnellier@gmail.com>
2020-11-17 12:00:39 +0000

    part1ofcontroller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,10 @@
 class BookingsController < ApplicationController
+
+  def edit
+  end
+
+  def destroy
+    @booking.destroy(booking_params)
+    redirect_to @booking, notice: "Your booking has been cancelled "
+  end
 end
