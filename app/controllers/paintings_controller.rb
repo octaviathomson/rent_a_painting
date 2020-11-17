f01b8126b574451bebf576b4817dddf21cb88e33
Diego Philips <diego.r.philips@gmail.com>
2020-11-17 11:06:03 +0000

    created the view

--- app/controllers/paintings_controller.rb
+++ app/controllers/paintings_controller.rb
@@ -1,2 +1,19 @@
 class PaintingsController < ApplicationController
+
+  def edit
+  end
+
+  def update
+    if @painting.update(painting_params)
+      redirect_to @painting, notice: 'Your painting is beautiful and ready to be displayed'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def painting_params
+    params.require(:painting).permit(:name, :price, :description, :user_id)
+  end
 end
